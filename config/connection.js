c5b53636b996ddddaa917c4eb5088f3c8c76efb5
fazilrahman <fazil2915@gmail.com>
2021-05-29 23:25:14 +0530

    new

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url="mongodb://localhost:27017"
-    const dbname="shopping"
+    const url="mongodb+srv://book1234:book2021@cluster0.y2gry.mongodb.net/shoppingcart"
+    const dbname="shoppingcart"
 
     mongoClient.connect(url,(err,data)=> {
         { useUnifiedTopology: true } 
