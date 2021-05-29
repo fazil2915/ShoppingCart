31011852857546a610ca53088644febafd533560
fazilrahman <fazil2915@gmail.com>
2021-05-29 23:44:00 +0530

    new

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.use(function(req, res, next) {
  * Get port from environment and store in Express.
  */
 
- var port = normalizePort(process.env.PORT || '8000');
- app.set('port', port);
+ /**var port = normalizePort(process.env.PORT || '8000');
+  app.set('port', port);
  
  // Create HTTP server.
   
